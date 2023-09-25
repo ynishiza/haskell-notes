b5593012c60a01db21d57690126bccacf2a67d6d
Yui Nishizawa <yui.nishizawa@gmail.com>
2023-09-25 09:42:15 +0900

    Pattern match with dependent type parameter

--- src/20230918patternSynonym.hs
+++ src/20230918patternSynonym.hs
@@ -1,13 +1,19 @@
 #!/usr/bin/env stack
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 {-
   Run with
     stack exec -- src/scratch/<name>.hs
     stack ghci -- src/scratch/<name>.hs
 -}
-
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE ViewPatterns #-}
-module AB ( pattern Empty) where
+
+module AB (pattern Empty) where
+
+import Data.Kind (Type)
 
 main :: IO ()
 main = do
@@ -42,14 +48,15 @@ empty = ListEmpty
 
 succ :: a -> List a -> List a
 succ a ListEmpty = ListSucc a ListEmpty 1
-succ a l@(ListSucc _ _ n)  = ListSucc a l (n + 1)
+succ a l@(ListSucc _ _ n) = ListSucc a l (n + 1)
 
 pattern Even :: Integral a => a -> a
 pattern Even n <- (\x -> if even x then Just x else Nothing -> Just n)
+
 pattern Odd :: Integral a => a -> a
 pattern Odd n <- (\x -> if odd x then Just x else Nothing -> Just n)
 
-{-# COMPLETE Even,Odd #-}
+{-# COMPLETE Even, Odd #-}
 
 printNum :: (Show a, Integral a) => a -> IO ()
 printNum (Even n) = putStrLn $ "Even:" <> show n
@@ -57,8 +64,51 @@ printNum (Odd n) = putStrLn $ "Odd:" <> show n
 
 -- pattern A a <- Just a
 pattern A :: Ord a => a -> a -> [a]
-pattern A a b <- [a, b] where 
-  A a b 
-    | a > b = [a]
-    | otherwise = [b]
+pattern A a b <- [a, b]
+  where
+    A a b
+      | a > b = [a]
+      | otherwise = [b]
+
 -- pattern B (a, a) <- Just a
+--
+
+data FileType = Txt | Log
+
+type SFileType :: FileType -> Type
+data SFileType filetype where
+  STxt :: SFileType 'Txt
+  SLog :: SFileType 'Log
+
+type File :: FileType -> Type
+data File filetype = File (SFileType filetype) FilePath
+
+-- pattern TxtFile :: FilePath -> File 'Txt       -- BAD. Cannot pattern match any File
+pattern TxtFile ::                                -- GOOD
+  forall (filetype :: FileType).
+  () =>                                           -- match any File
+  (filetype ~ 'Txt) =>                            -- construct File 'Txt
+  FilePath ->
+  File filetype
+pattern TxtFile p = File STxt p
+
+-- pattern LogFile :: FilePath -> File 'Txt       -- BAD. Cannot pattern match any File
+pattern LogFile ::                                -- GOOD
+  forall (filetype :: FileType).
+  () =>                                           -- match any File
+  (filetype ~ 'Log) =>                            -- construct File 'Log
+  FilePath ->
+  File filetype
+pattern LogFile p = File SLog p
+
+{-# COMPLETE TxtFile, LogFile #-}
+
+instance Show (File filetype) where
+  show (TxtFile path) = "TxtFile " <> path
+  show (LogFile path) = "LogFile " <> path
+
+textFile :: File 'Txt
+textFile = TxtFile "a.txt"
+
+logFile :: File 'Log
+logFile = LogFile "a.log"
